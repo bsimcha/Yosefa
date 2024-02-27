a5bdcede9c7ec2873bbade74b34946d529e1d2da
Baila Simcha <bailasimcha@gmail.com>
2024-02-26 20:55:52 -0600

    adds index action for categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   #PAINTING
   get "/paintings" => "paintings#index"
   get "/paintings/:id" => "paintings#show"
+
+  #CATEGORY
+  get "/categories" => "categories#index"
 end
