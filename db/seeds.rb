1ee48d021830e60fc54390d3c4dcddd1f82f7a19
Baila Simcha <bailasimcha@gmail.com>
2024-02-28 20:05:25 -0600

    adds seed data to categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 # Painting.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWq-VofnQYns9YKDArJGbojQqKsbOwlmoPvfx8NqDIjg&s", description: "Sun rays on Kosel", price: 45000)
 
 # Category.create(name: "Original")
+Category.create(name: "Semi-Originals")
+Category.create(name: "Custom Commissioned Orders")
+Category.create(name: "Waterdale Collab Originals")
