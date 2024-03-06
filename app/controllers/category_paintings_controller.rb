ae18128656426b72aa1f4ec2279177d474827525
askarf <ayalakarf@gmail.com>
2024-03-05 21:13:35 -0500

    adds uniqness to categorypainting

--- app/controllers/category_paintings_controller.rb
+++ app/controllers/category_paintings_controller.rb
@@ -1,10 +1,14 @@
 class CategoryPaintingsController < ApplicationController
   def create
-    @category_painting = CategoryPainting.create(
+    @category_painting = CategoryPainting.new(
       painting_id: params[:painting_id],
       category_id: params[:category_id],
     )
-    render :index
+    if @category_painting.save
+      render json: { message: "Painting Catigorized Successfully" }, status: :created
+    else
+      render json: { errors: @category_painting.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def index
--- app/models/category_painting.rb
+++ app/models/category_painting.rb
@@ -1,4 +1,5 @@
 class CategoryPainting < ApplicationRecord
+  validates :category_id, uniqueness: { scope: :painting_id, message: "can only be used once per painting" }
   belongs_to :category
   belongs_to :painting
 end
