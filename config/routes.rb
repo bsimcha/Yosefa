6c8f7786f731e420feade404aaa3541446b5764c
adinareiter <144619417+adinareiter@users.noreply.github.com>
2024-03-07 19:37:21 -0500

    Add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   #CATEGORY_PAINTING
   get "/category_paintings" => "category_paintings#index"
   post "/category_paintings" => "category_paintings#create"
+
+  #USERS
+  post "/users" => "users#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Painting.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWq-VofnQYns9YKDArJGbojQqKsbOwlmoPvfx8NqDIjg&s", description: "Sun rays on Kosel", price: 45000)
+# Painting.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWq-VofnQYns9YKDArJGbojQqKsbOwlmoPvfx8NqDIjg&s", description: "Sun rays on Kosel")
 
 # Category.create(name: "Original")
 # Category.create(name: "Semi-Originals")
