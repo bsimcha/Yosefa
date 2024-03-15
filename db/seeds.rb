84d05cd712a298fbea82b24eedad1d9f58485a71
adinareiter <144619417+adinareiter@users.noreply.github.com>
2024-03-15 15:44:52 -0400

    creates image instance with local image

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@
 # Category.create(name: "Semi-Originals")
 # Category.create(name: "Custom Commissioned Orders")
 # Category.create(name: "Waterdale Collab Originals")
+
+Image.create(url: "/Users/adinareiter/Actualize/Yosefa/bin/kever-rochel.jpg", painting_id: 1)
