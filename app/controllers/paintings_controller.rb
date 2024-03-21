c19850688506d535c791b00eb7edeab0df6d3925
Baila Simcha <bailasimcha@gmail.com>
2024-03-20 19:56:42 -0500

    commented out second create action for paintings

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -9,37 +9,37 @@ class PaintingsController < ApplicationController
     render :show
   end
 
-  def create
-    @painting = Painting.create(
-      description: params[:description],
-    )
-    render :show
-  end
-
   # def create
-  #   if params[:url1].blank?
-  #     render json: { errors: ["At least one image is required."] }, status: :unprocessable_entity
-  #     return
-  #   end
-
   #   @painting = Painting.create(
-
   #     description: params[:description],
-
   #   )
-
-  #   (1..20).each do |i|
-  #     if params["url#{i}"].present?
-  #       @painting.images.build(url: params["url#{i}"])
-  #     end
-  #   end
-
-  #   if @painting.save
-  #     render json: { message: "Painting successfully created!" }, status: :created
-  #   else
-  #     render json: { errors: @painting.errors.full_messages }, status: :unprocessable_entity
-  #   end
+  #   render :show
   # end
+
+  def create
+    if params[:url1].blank?
+      render json: { errors: ["At least one image is required."] }, status: :unprocessable_entity
+      return
+    end
+
+    @painting = Painting.create(
+
+      description: params[:description],
+
+    )
+
+    (1..20).each do |i|
+      if params["url#{i}"].present?
+        @painting.images.build(url: params["url#{i}"])
+      end
+    end
+
+    if @painting.save
+      render json: { message: "Painting successfully created!" }, status: :created
+    else
+      render json: { errors: @painting.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   def update
     @painting = Painting.find_by(id: params[:id])
