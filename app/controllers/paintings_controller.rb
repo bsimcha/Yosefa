347db66bfaf7c304c1b5aa074a5de9fa30ac286c
Baila Simcha <bailasimcha@gmail.com>
2024-09-12 15:18:53 -0500

    clean up

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -45,55 +45,9 @@ class PaintingsController < ApplicationController
     end
   end
 
-  # def create
-  #   @painting = Painting.create(
-  #     description: params[:description],
-  #   )
-  #   render :show
-  # end
-
-  # def create
-  #   if params[:url1].blank?
-  #     render json: { errors: ["At least one image is required."] }, status: :unprocessable_entity
-  #     return
-  #   end
-
-  # def create
-  #   @painting = Painting.create(
-  #     description: params[:description],
-  #   )
-  #   render :show
-  # end
-
-  #   def create
-  #     if params[:url1].blank?
-  #       render json: { errors: ["At least one image is required."] }, status: :unprocessable_entity
-  #       return
-  #     end
-
-  #     @painting = Painting.create(
-
-  #       description: params[:description],
-
-  #     )
-
-  #     (1..20).each do |i|
-  #       if params["url#{i}"].present?
-  #         @painting.images.build(url: params["url#{i}"])
-  #       end
-  #     end
-
-  #     if @painting.save
-  #       render json: { message: "Painting successfully created!" }, status: :created
-  #     else
-  #       render json: { errors: @painting.errors.full_messages }, status: :unprocessable_entity
-  #     end
-  #   end
-
   def update
     @painting = Painting.find_by(id: params[:id])
     @painting.update(
-      # image: params[:image] || @painting.image,
       description: params[:description] || @painting.description,
     )
     render :show
--- app/views/category_paintings/index.json.jbuilder
+++
@@ -1,6 +0,0 @@
-json.array! @category_paintings do |category_painting|
-  json.id category_painting.id
-  json.painting_id category_painting.painting_id
-  json.category_id category_painting.category_id
-  
-end
